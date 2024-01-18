321033391824365d7a89f77d3a5d448f19a27f88
felixowolabi <flo.owol@gmail.com>
2024-01-18 23:51:05 +0200

    changes

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
-import React, { useState, useEffect } from "react";
-import axios from "../axios";
+import React from "react";
+import react, { useState, useEffect } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+// you can only have one default export in a file
+//fetchUrl was pass in the function below as a prop
+function Row({ title, fetchUrl }) {
   const [movie, setMovies] = useState([]);
-  // A snippet of code which runs based on a specific condition
+  // A snippet of code which runs based on a specific condition /variable
   useEffect(() => {}, [movie]);
   //if [], run once when th row loads, and dont run again.
   //if Array is empty load the condition once but when there is a parameter for example " movie" load when movie changes.
+  async function fetchData() {
+    const request = await axios.get(fetchUrl);
+    console.log(request);
+  }
+  fetchData();
   return (
     <div>
       <h2> {title}</h2>
