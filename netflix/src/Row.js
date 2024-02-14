feb39ebf759d416d2bc5d13f81930514cc4a2566
felixowolabi <flo.owol@gmail.com>
2024-02-14 09:06:44 +0200

    created banner and styling

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -7,7 +7,7 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 
 // you can only have one default export in a file
 //fetchUrl was pass in the function below as a prop
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [Movies, setMovies] = useState([]);
   // A snippet of code which runs based on a specific condition /variable
   useEffect(() => {
@@ -21,15 +21,17 @@ function Row({ title, fetchUrl }) {
 
     fetchData();
   }, [fetchUrl]);
-  console.table(Movies);
   return (
     <div className="row">
       <h2> {title}</h2>
       <div className="row__posters">
         {Movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
